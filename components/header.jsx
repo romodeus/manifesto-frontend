f3d0d656c5c1807b8a7dd62467f9de254732a4ad
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-23 22:00:20 +0700

    feat(form): fix bugs and add responsive

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className='tracking-[.12em] text-center text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 pb-2'>MANIFESTO</h1>
-      <p className='text-center text-xl font-bold text-[#2C2C2C] tracking-wider'>Make your <span className='text-[#BD279C]'>URL</span> easier to remember.</p>
+    <div className="pt-20">
+      <div className="flex justify-center">
+        <div className="col tracking-[.12em] text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 pb-2">
+          MANIFESTO
+        </div>
+      </div>
+      <p className="text-center text-xl font-bold text-[#2C2C2C] tracking-wider">
+        Make your <span className="text-[#BD279C]">URL</span> easier to
+        remember.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ components/link.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+
+const Link = () => {
+  const data = useRouter();
+  const [shorturl, setShortUrl] = useState([]);
+
+  const getUrl = () => {
+    axios
+      .get(`https://man.romodeus.site/manifest/${data.query.key}`)
+      .then((response) => {
+        console.log(response);
+        setShortUrl(response.data.data.custom_url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getUrl();
+  }, []);
+
+  return (
+    <div className="pt-32 md:pt-40 xl:pt-16">
+      <div className="flex justify-center">
+        <div className="xl:w-96 w-72 md:w-80">
+          <div className="border border-black py-6 w-full rounded">
+            <p className="text-black text-xs font-semibold">{shorturl}</p>
+          </div>
+          <div className="flex justify-center pt-8">
+            <button
+              className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-300 px-5 py-2 rounded text-sm"
+              onClick={() => navigator.clipboard.writeText({ shorturl })}
+            >
+              Copy <span className="text-[#BD279C]">URL</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Link;
